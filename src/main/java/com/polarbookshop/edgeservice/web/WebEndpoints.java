ff61b0c12892ef4df608848cb68e2114787462f5
Sean Maxwell <sa.maxwell@yahoo.com>
2023-05-02 23:47:08 -0500

    Added rate limiting with Redis

--- src/main/java/com/polarbookshop/edgeservice/web/WebEndpoints.java
+++ src/main/java/com/polarbookshop/edgeservice/web/WebEndpoints.java
@@ -3,7 +3,6 @@ package com.polarbookshop.edgeservice.web;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
